76b66f558a5fc8a7be12512b91eca78d2eecfb9d
matthewfro1717 <114807610+matthewfro1717@users.noreply.github.com>
2023-06-11 17:25:33 -0400

    Update PlayState.hx

--- source/PlayState.hx
+++ source/PlayState.hx
@@ -1548,8 +1548,11 @@ class PlayState extends MusicBeatState
 	public function reloadHealthBarColors() {
 		healthBar.createFilledBar(FlxColor.fromRGB(dad.healthColorArray[0], dad.healthColorArray[1], dad.healthColorArray[2]),
 			FlxColor.fromRGB(boyfriend.healthColorArray[0], boyfriend.healthColorArray[1], boyfriend.healthColorArray[2]));
-
-		healthBar.updateBar();
+			
+			timeBar.createFilledBar(0xFF121212, FlxColor.fromRGB(dad.healthColorArray[0], dad.healthColorArray[1], dad.healthColorArray[2]));
+	
+			healthBar.updateBar();
+			timeBar.updateBar();
 	}
 
 	public function addCharacterToList(newCharacter:String, type:Int) {
